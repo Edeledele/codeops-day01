296ff1ce0fd99973a3dfb7120bad01ad28091cbe
GenetTilahun <genettilahun222.com>
2026-07-24 17:35:07 +0300

    Day 4: exercises mini project update

--- module1/day4_exercises/day4_mini_project.py
+++ module1/day4_exercises/day4_mini_project.py
@@ -1,155 +1,29 @@
-# Addis Bank Account System
-# Mini Project
-# This is the only file using input()
 
+# Day 4 -> Encapsulated BankAccount (private balance via property)
 
 class BankAccount:
-
-
-    def __init__(self,name,balance):
-
-        self.name=name
-
-        self.__balance=balance
-
-
+    def __init__(self, number, name, balance=0):
+        self.number = number
+        self.name = name
+        self._balance = balance
 
     @property
     def balance(self):
+        return self._balance
 
-        return self.__balance
+    def deposit(self, amount):
+        if amount > 0:
+            self._balance += amount
 
-
-
-    def deposit(self,amount):
-
-        if amount>0:
-
-            self.__balance+=amount
-
-
-
-    def withdraw(self,amount):
-
-        if amount <= self.__balance:
-
-            self.__balance-=amount
-
-        else:
-
+    def withdraw(self, amount):
+        if amount > self._balance:
             print("Not enough balance")
-
-
-
-
-
-accounts={}
-
-
-
-while True:
-
-
-    print("\nAddis Bank")
-
-    print("1.Create Account")
-
-    print("2.Deposit")
-
-    print("3.Withdraw")
-
-    print("4.Check Balance")
-
-    print("5.View Account")
-
-    print("6.Exit")
-
-
-
-    choice=input("Choose: ")
-
-
-
-
-    if choice=="1":
-
-        number=input("Account number: ")
-
-        name=input("Name: ")
-
-        account=BankAccount(name,0)
-
-        accounts[number]=account
-
-
-
-        print("Account Created")
-
-
-
-
-    elif choice=="2":
-
-        number=input("Account number: ")
-
-        amount=int(input("Amount: "))
-
-
-        accounts[number].deposit(amount)
-
-
-
-
-    elif choice=="3":
-
-        number=input("Account number: ")
-
-        amount=int(input("Amount: "))
-
-
-        accounts[number].withdraw(amount)
-
-
-
-
-    elif choice=="4":
-
-        number=input("Account number: ")
-
-
-        print(
-        accounts[number].balance
-        )
-
-
-
-
-    elif choice=="5":
-
-        number=input("Account number: ")
-
-
-        print(
-        "Owner:",
-        accounts[number].name
-        )
-
-
-        print(
-        "Balance:",
-        accounts[number].balance
-        )
-
-
-
-
-    elif choice=="6":
-
-        break
-
-
-
-    else:
-
-        print("Wrong option")
-        
+            return
+        self._balance -= amount
+
+
+if __name__ == "__main__":
+    acc = BankAccount("001", "Abebe", 500)
+    acc.deposit(200)
+    acc.withdraw(100)
+    print(acc.name, acc.balance)
